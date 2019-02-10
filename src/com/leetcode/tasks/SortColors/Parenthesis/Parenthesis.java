4d33f36acd653d63c97dd64f0f273e1b72b6807c
lockdain <lockdain@gmail.com>
2019-02-10 15:45:17 +0300

    - Stepik implementation of parenthesis balancing

--- src/com/leetcode/tasks/SortColors/Parenthesis/Parenthesis.java
+++ src/com/leetcode/tasks/SortColors/Parenthesis/Parenthesis.java
@@ -5,6 +5,12 @@ import java.util.Objects;
 
 public class Parenthesis {
 
+    /**
+     * Метод проверяет, является ли входная последовательность скобок сбалансированной.
+     *
+     * @param s
+     * @return
+     */
     public static boolean isBalanced(String s) {
         if (Objects.isNull(s)) {
             throw new NullPointerException("String is null");
@@ -40,8 +46,62 @@ public class Parenthesis {
         return stack.isEmpty();
     }
 
+    /**
+     * Реализация метода для сайта stepik
+     *
+     * @param s
+     */
+    public static void isBalanced(String[] s) {
+        if (Objects.isNull(s)) {
+            throw new NullPointerException("String is null");
+        }
+
+        if (s.length == 0) {
+            System.out.println("Success");
+            return;
+        }
+
+        if (s.length == 1) {
+            System.out.println("1");
+            return;
+        }
+
+        ArrayDeque<String> stack = new ArrayDeque<>();
+
+        int c = 0;
+        do {
+            String e = s[c];
+            if (String.valueOf(e).equals("[")
+                    || String.valueOf(e).equals("(")
+                    || String.valueOf(e).equals("{")) {
+                stack.push(e);
+                c++;
+            } else if (!stack.isEmpty() && String.valueOf(e).equals("]") && stack.peek().equals("[")) {
+                stack.pop();
+                c++;
+            } else if (!stack.isEmpty() && String.valueOf(e).equals(")") && stack.peek().equals("(")) {
+                stack.pop();
+                c++;
+            } else if (!stack.isEmpty() && String.valueOf(e).equals("}") && stack.peek().equals("{")) {
+                stack.pop();
+                c++;
+            } else {
+                System.out.println(++c);
+                return;
+            }
+        } while (c != s.length);
+        if (stack.isEmpty())
+            System.out.println("Success");
+    }
+
 
     public static void main(String[] args) {
-        System.out.println(isBalanced("(]"));
+//        isBalanced(new String[]{"(", ")", "[", "]", "}", "}"});
+//        isBalanced(new String[]{"(", "[", "]", "(", ")", "{", "(", "[", "]", ")", "}", ")"});
+//        isBalanced(new String[]{"{", "{", "[", "(", ")", "]", "]"});
+//        isBalanced(new String[]{"(", ")", "[", "]", "}"});
+
+
     }
 }
+
