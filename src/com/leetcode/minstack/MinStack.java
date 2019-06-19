2b90fd1a8da321d69048c5038a105b05fea6fe3e
lockdain <lockdain@gmail.com>
2019-06-19 09:39:29 +0300

    Comments added.

--- src/com/leetcode/minstack/MinStack.java
+++ src/com/leetcode/minstack/MinStack.java
@@ -3,6 +3,24 @@ package com.leetcode.minstack;
 
 import java.util.ArrayDeque;
 
+/**
+ * Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+ * <p>
+ * push(x) -- Push element x onto stack.
+ * pop() -- Removes the element on top of the stack.
+ * top() -- Get the top element.
+ * getMin() -- Retrieve the minimum element in the stack.
+ *
+ * Example:
+ * MinStack minStack = new MinStack();
+ * minStack.push(-2);
+ * minStack.push(0);
+ * minStack.push(-3);
+ * minStack.getMin();   --> Returns -3.
+ * minStack.pop();
+ * minStack.top();      --> Returns 0.
+ * minStack.getMin();   --> Returns -2.
+ */
 public class MinStack {
     private final ArrayDeque<Integer> minStory = new ArrayDeque<>();
     private final ArrayDeque<Integer> arrayList = new ArrayDeque<>();
@@ -12,7 +30,7 @@ public class MinStack {
     /**
      * initialize your data structure here.
      */
-     MinStack() {
+    MinStack() {
 
     }
 
